4860da4588bed5e1682ee89837b4450b5a543491
BIONIChae <chanteldhlamini12@gmail.com>
2023-02-16 12:01:15 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Mandatory function which controls the execution of the program.
+ *
+ * Return: 0 = No errors identified. Non- 0 = Errors detacted.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
