cc784fee999b2cf92d34ce469af69c3948a963af
BIONIChae <chanteldhlamini12@gmail.com>
2023-02-16 12:09:24 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main -mandatory primary function which helps with execution.
+ *
+ * Return: 0 without errors, non- 0 with errors.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
