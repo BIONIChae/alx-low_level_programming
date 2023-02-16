38f735825a20ba79621a52b028e356c110795191
BIONIChae <chanteldhlamini12@gmail.com>
2023-02-16 13:18:00 -0800

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main -Primary function for execution.
+ *
+ * Return: 0 when no errors are found. Else, non-0.
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
