74920723e44366bc5cc80e29272a2cb225e33bc1
BIONIChae <chanteldhlamini12@gmail.com>
2023-02-17 11:45:51 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - print numbers from 0 to base 10
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+	printf("\n");
+	return (0);
+}
