cc00dd3c222ca2df39c87bd2b8f52b5bbc987086
BIONIChae <chanteldhlamini12@gmail.com>
2023-02-18 07:58:56 -0800

    Different combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - is entry of program
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 8; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
