22f5974d945382f24d96a2fadcec65a47711ec8e
BIONIChae <chanteldhlamini12@gmail.com>
2023-02-23 11:56:35 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle using '#'.
+ * @size:  The size of the triangle.
+ */
+void print_triangle(int size)
+{
+	int h, w;
+
+	if (size > 0)
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (w = size - h; w > 0; w--)
+				_putchar(' ');
+
+			for (w = 0; w < h; w++)
+				_putchar('#');
+
+			if (h == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
