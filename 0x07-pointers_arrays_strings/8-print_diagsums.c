8664bd336ba73c6a9cf386bc82c5ffca7bd968aa
BIONIChae <chanteldhlamini12@gmail.com>
2023-03-06 15:19:01 -0800

    Chess

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * print_diagsum - Prints the sum of the two diagonals
+ *		of a square matrix of integers.
+ * @a: square matrix of integers.
+ * @size: size of the matrix.
+ */
