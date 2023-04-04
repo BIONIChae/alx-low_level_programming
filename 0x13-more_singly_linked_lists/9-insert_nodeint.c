6e37aa0c8babf01393d18e949d045a64c1c777e9
BIONIChae <chanteldhlamini12@gmail.com>
2023-04-04 14:17:33 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,17 +16,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
-
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-
 	ptr = *head;
 	while (node < idx - 1 && ptr != NULL)
 	{
