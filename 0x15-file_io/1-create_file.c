c72e36ee9bd967e9ff7a1d624586e139cf1da2c7
BIONIChae <chanteldhlamini12@gmail.com>
2023-04-10 14:39:01 -0700

    Creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - Curates a file.
+ * @filename: Name of the file.
+ * @text_content: A NULL string to write the file.
+ *
+ * Return: On success 1, else -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int length = 0;
+	int file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (; text_content[length]; length++)
+			;
+	}
+
+	close(file);
+	return (1);
+}
