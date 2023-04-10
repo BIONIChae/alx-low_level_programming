7df6e51d8efdd72b12a1efeede30148723cbc42b
BIONIChae <chanteldhlamini12@gmail.com>
2023-04-10 15:55:31 -0700

    Creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int length = 0;
 	int file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	int w_file = write(file, text_content, length);
 
 	if (filename == NULL)
 		return (-1);
@@ -20,8 +21,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (; text_content[length];)
-			length++;
+		if (w_file == -1)
+		{
+			close(file);
+			return (-1);
+		}
 	}
 
 	close(file);
